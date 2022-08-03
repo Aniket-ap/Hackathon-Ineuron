a3fec81fc64ff619a33b37383474b975ebd995db
Aniket Pradhan <aniketpradhan814@gmail.com>
2022-08-03 20:18:26 +0530

    js file added

---
+++ index.js
@@ -0,0 +1,13 @@
+// var sections = document.querySelectorAll("div");
+// var container = document.querySelector(".cards__course");
+// container.addEventListener("scroll", function () {
+//   sections.forEach((e, i) => {
+//     var top = e.getBoundingClientRect().top;
+//     if (top == 0) {
+//       e.classList.add("lineUpRight");
+//     } else {
+//       e.classList.remove("lineUpRight");
+//     }
+//   });
+// });
+// container.dispatchEvent(newCustomEvent("scroll"));
