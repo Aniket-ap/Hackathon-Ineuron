6186011d4d10fb92b68d6ea00a892e901716d1ee
Aniket Pradhan <aniketpradhan814@gmail.com>
2022-08-04 10:36:20 +0530

    added smooth scroll effect

--- index.js
+++ index.js
@@ -1,31 +1,7 @@
-// var sections = document.querySelectorAll("div");
-// var container = document.querySelector(".cards__course");
-// container.addEventListener("scroll", function () {
-//   sections.forEach((e, i) => {
-//     var top = e.getBoundingClientRect().top;
-//     if (top == 0) {
-//       e.classList.add("lineUpRight");
-//     } else {
-//       e.classList.remove("lineUpRight");
-//     }
-//   });
-// });
-// container.dispatchEvent(newCustomEvent("scroll"));
-
-// function myFunction() {
-//     console.log("asfdsf");
-//     var x = document.getElementById("myTopnav");
-//     if (x.className === "ulnav") {
-//       x.className += " responsive";
-//     } else {
-//       x.className = "ulnav";
-//     }
-// }
-
-const button = document.querySelector('.ham');
-button.addEventListener('click', addClass, false);
+const button = document.querySelector(".ham");
+button.addEventListener("click", addClass, false);
 
 function addClass() {
-  var x = document.querySelector('.res__none');
-  x.classList.toggle('res__ul');
+  var x = document.querySelector(".res__none");
+  x.classList.toggle("res__ul");
 }
