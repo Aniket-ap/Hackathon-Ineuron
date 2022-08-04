91f1a5f0fe4129fb941cd9ee96a8d3f0cfb36e9a
Aniket Pradhan <aniketpradhan814@gmail.com>
2022-08-04 09:47:02 +0530

    added responsive nav list

--- index.js
+++ index.js
@@ -21,3 +21,11 @@
 //       x.className = "ulnav";
 //     }
 // }
+
+const button = document.querySelector('.ham');
+button.addEventListener('click', addClass, false);
+
+function addClass() {
+  var x = document.querySelector('.res__none');
+  x.classList.toggle('res__ul');
+}
